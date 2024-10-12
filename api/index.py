2f0c37ee4cdebe8d4113549d6d7c5c0e73c7921a
YashedP <yashjani144@gmail.com>
2024-10-12 02:37:29 -0400

    backend api testing

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/api/login', methods=['POST'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
 
-@app.route('/about')
-def about():
-    return 'About'
+    if username == None or password == None:
+        return(jsonify({'status': 'error', 'message': 'Username or password not provided'}))
+    return(jsonify({'username': username, 'password': password, 'status': 'success'}))
